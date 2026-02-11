98f43797d7840cd6cdc5fd7b8a4cd463e23e7109
Catalina-M-Quality <catalinamossoscastro@gmail.com>
2026-02-11 14:52:22 -0500

    report-config

---
+++ cypress/e2e/first-test.cy.js
@@ -0,0 +1,7 @@
+//Hola desde mi pc
+describe('Conjunto de pruebas iniciales', () => {
+  it('Mi primera prueba en Cypress', () => {
+    cy.log('Hola! Estoy ejecutando mi primer test para el reporte de Allure');
+    cy.log('Prueba finalizada con éxito');
+  });
+});
